ec6664795d79c2b07b0237b54a0189b35f604157
Jonny <butasioan@gmail.com>
2026-05-12 03:33:06 +0200

    add brand red visual line

---
+++ components/BrandRedLine.tsx
@@ -0,0 +1,7 @@
+export default function BrandRedLine() {
+  return (
+    <div className="my-10 h-1 w-full overflow-hidden rounded-full bg-neutral-200">
+      <div className="h-full w-2/3 rounded-full bg-red-600" />
+    </div>
+  );
+}
